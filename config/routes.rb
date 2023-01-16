6cd22191adbb3c5ca03273e72984247e83dc2815
Pbeckmann94 <thebeckmann@hotmail.com>
2023-01-16 15:54:16 +0100

    litt oversatt til engelsk og spansk

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,25 @@
 Rails.application.routes.draw do
+  get 'admin' => 'admin#index'
+  controller :sessions do
+    get 'login' => :new
+    post 'login' => :create
+    delete 'logout' => :destroy
+  end
+
+  get 'sessions/create'
+  get 'sessions/destroy'
+  resources :users
+  resources :products
+scope '(:locale)' do
   resources :orders
   resources :line_items
   resources :carts
-  root 'store#index', as: 'store_index' 
-  resources :products
+  root 'store#index', as: 'store_index', via: :all
+ 
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+end
 end
